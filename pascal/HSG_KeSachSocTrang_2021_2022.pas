285bbaf2167ffc7acc82eb9a2c649bf24ee1cf04
Chánh <phannhatchanh@gmail.com>
2022-02-18 22:17:09 +0700

    Create HSG_KeSachSocTrang_2021_2022.pas

---
+++ pascal/HSG_KeSachSocTrang_2021_2022.pas
@@ -0,0 +1,10 @@
+{
+Viết chương trình nhập vào một dãy số gồm n số nguyên (n>0).
+a) Tìm số lớn nhất vàn vị trí của số lớn nhất trong dãy số vừa nhập (Nếu có nhiều số lớn nhất bằng nhau thì in ra
+vị trí của số lớn nhất sau cùng). (Ví dụ: Nhập vào 5 15 23 1 -2 in ra 23 và 3)
+b) Chèn thêm một số k vào vị trí m (k và m được nhập từ bàn phím). In ra dãy số có chứa số, vị trí vừa nhập. (Ví dụ: Chèn số 7 vào
+   vị trí thứ 2. Ta được dãy số mới 5 7 15 23 1 -2)
+c) Các số nguyên tố có trong mảng. (KQ: 5 7 23)
+d) Các phần tử được sắp xếp theo thứ tự tăng dần. (KQ: -2 1 5 7 15 23)
+e) Phần tử có giá trị tuyệt đối lớn nhất có trong mảng. (KQ: 23)
+}
