edf7d554344d451d9ef8b4274332caf4016027e0
Chánh <phannhatchanh@gmail.com>
2022-02-22 18:30:14 +0700

    Update and rename pascal/string/Tìm mật khẩu/index.md to pascal/string/tim_mat_khau.pas

---
+++ pascal/string/tim_mat_khau.pas
@@ -0,0 +1,17 @@
+{
+  Việc bảo vệ máy tính để hạn chế người khác thâm nhập là một vấn đề đặt ra cho mọi người sử dụng máy tính.
+Để tăng tính an toàn trong lưu trữ, một người đã quyết định giấu mật khẩu truy cập máy tính của mình vào
+một xâu S với một qui ước sao cho khi cần anh ta có thể lấy lại được mật khẩu từ xâu S. Là một người yêu 
+thích số học anh ta chọn mật khẩu P là một số tự nhiên và mã hóa P=P1+P2+...+Pi (Với Pi là số nguyên, 0<=Pi<=9 và 0<i<200)
+Đem dấu các số Pi vào một xâu kí tự S.
+  Chẳng hạn xâu S='mat #223 ma 678 @' chứa mật khẩu là 28 vì P=2+2+3+6+7+8
+  Yêu cầu: Cho một xâu kí tự S chiều dài không quá 255 kí tự. Tìm mật khẩu P đã dấu trong S.
+}
+
+
+
+
+
+
+
+
