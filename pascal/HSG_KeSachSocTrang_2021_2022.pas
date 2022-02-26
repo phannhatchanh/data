5b53f026a609a958e40c6587f9e5d440e19e1340
Chánh <phannhatchanh@gmail.com>
2022-02-26 15:22:28 +0700

    Update HSG_KeSachSocTrang_2021_2022.pas

--- pascal/HSG_KeSachSocTrang_2021_2022.pas
+++ pascal/HSG_KeSachSocTrang_2021_2022.pas
@@ -8,3 +8,66 @@ c) Các số nguyên tố có trong mảng. (KQ: 5 7 23)
 d) Các phần tử được sắp xếp theo thứ tự tăng dần. (KQ: -2 1 5 7 15 23)
 e) Phần tử có giá trị tuyệt đối lớn nhất có trong mảng. (KQ: 23)
 }
+
+Uses Crt;
+Var N,i,Max,vt,k,m,j,d,dem:Integer;
+    A: array[1..100] of Integer;
+Begin
+Clrscr;
+  Write(' Nhap so phan tu cua mang : '); Readln(N);
+  For i:=1 to N do
+    Begin
+      Write('Nhap phan tu thu ', i,' '); Readln(A[i]);
+    End;
+  For i:=1 to N do
+    Begin
+      Write(A[i],' ');
+    End;
+  Max:=A[1];
+  vt:=1;
+  For i:=2 to N do
+    If Max<A[i] then
+       Begin
+         Max:=A[i];
+         vt:=i;
+       End;
+  Writeln('So lon nhat trong day so la: ',Max,' vi tri la: ',vt);
+  Write('Nhap so can chen: '); Readln(k);
+  Write('Nhap vi tri can chen: '); Readln(m);
+  For i:=N downto m do
+    A[i+1]:=A[i];
+  A[m]:=k;
+  N:=N+1;
+  For i:=1 to N do
+    Write(A[i],' ');
+  Writeln;
+  dem:=0;
+  For i:=1 to N do
+    Begin
+      d:=0;
+      For j:=1 to A[i] do
+        If A[i] mod j=0 then d:=d+1;
+      If d=2 then Begin dem:=dem+1;Writeln(A[i],' '); End;
+
+    End;
+
+
+
+
+
+
+
+
+
+
+Readln;
+End.
+
+
+
+
+
+
+
+
+
