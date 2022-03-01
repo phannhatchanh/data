55d0371274d6117937e8aeefa40d0a53b4647185
Chánh <phannhatchanh@gmail.com>
2022-03-01 19:33:58 +0700

    Create Day_so.pas

---
+++ pascal/Day_so.pas
@@ -0,0 +1,21 @@
+{
+Cho số tự nhiên N.
+  a) N có bao nhiêu chữ số.
+  b) Tổng các chữ số chẵn của N bằng bao nhiêu?
+  c) Tìm chữ số K của N kể từ vị trí đầu tiên bên trái (với K<= số chữ số của N)?
+  YÊU CẦU:
+    + Dữ liệu vào: Cho trong tệp văn bản BAI1.INP gồm hai số tự nhiên N, K. Các số cách nhau một khoảng trống.
+    + Dữ liệu ra: Ghi vào tệp văn bản BAI1.OUT gồm ba dòng, mỗi dòng một số là kết quả của các câu a, b và c.
+  VÍ DỤ:
+    BAI1.INP            BAI1.OUT
+    120346786 4         9
+                        26
+                        3
+}
+
+uses crt;
+var 
+
+
+
+
