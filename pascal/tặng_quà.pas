5cbb7979c62195a42f64f2f93b36983fe3dcb195
Chánh <phannhatchanh@gmail.com>
2022-03-01 22:04:17 +0700

    Update tặng_quà.pas

--- pascal/tặng_quà.pas
+++ pascal/tặng_quà.pas
@@ -1,8 +1,26 @@
 
+{
+    Tất cả khách hàng của một siêu thị Maximax đều có thẻ thành viên và được tích lũy điểm qua mỗi lần mua hàng.
+    Nhân dịp kỷ niệm 5 năm ngày khai trương. Siêu thị có chương trình tặng quà tri ân khách hàng. Có N người xếp
+    hàng chờ nhận quà, BTC thông báo những người có số điểm là bội của 3 sẽ rời khỏi hàng, những người còn lại
+    tịnh tiến lên đầu hàng một vị trí và giữ nguyên thứ tự để nhận quà.
+    YÊU CẦU: Hãy thông báo danh sách điểm những người được nhận quà theo thứ tự xếp hàng.
+    DỮ LIỆU VÀO: Cho file TANGQUA.INP gồm 2 dòng:
+      - Dòng đầu là số nguyên dương N cho biết số khách xếp hàng (0<N<=1000);
+      - Dòng 2 gồm N số nguyên là số điểm tương ứng của N khách hàng. Các số ghi cách nhau ít nhất một dấu cách.
+    KẾT QUẢ: Ghi ra file TANGQUA.OUT gồm một dòng là điểm của những người còn lại trong hàng theo đứng thứ tự vị
+    trí xếp hàng.
+    VÍ DỤ:
+      TANGQUA.INP               TANGQUA.OUT
+      10                        7 8 2 2 1
+      9 7 8 6 0 3 9 2 2 1
+}
 
-
-
-
+{
+    Ý TƯỞNG:
+      - Đọc file
+      - Lấy từng phần tử trong dãy mod 3 (Nếu không chia hết cho 3 thì ỉn ra)
+}
 
 
 
