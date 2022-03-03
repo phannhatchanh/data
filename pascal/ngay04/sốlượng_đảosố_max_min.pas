aad91dd661f1e9a71cecb907b9994fb6441760f8
Chánh <phannhatchanh@gmail.com>
2022-03-03 19:58:07 +0700

    Create sốlượng_đảosố_max_min.pas

---
+++ pascal/ngay04/sốlượng_đảosố_max_min.pas
@@ -0,0 +1,13 @@
+{
+  Nhập vào một số N
+  a) In số lượng chữ số của N
+  b) Số đảo của N
+  c) Chữ số lớn nhất, nhỏ nhất của N
+  VÍ DỤ: Nhập vào số N là 12345678901234567890123456789
+        - Số lượng: 29
+        - Số đảo: 98765432109876543210987654321
+        - Lớn nhất: 9; Nhỏ nhất: 0;
+}
+
+
+
