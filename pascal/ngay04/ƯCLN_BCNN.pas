3bb6a253a534774492edcd41c32ee29e32cb6394
Chánh <phannhatchanh@gmail.com>
2022-03-03 20:35:28 +0700

    Update ƯCLN_BCNN.pas

--- pascal/ngay04/ƯCLN_BCNN.pas
+++ pascal/ngay04/ƯCLN_BCNN.pas
@@ -5,6 +5,17 @@
 
 
 //Bài 1: Tìm ƯỚC của một số nguyên N
+uses crt;
+var i,n:longint;
+begin
+clrscr;
+   write('N = ');readln(n);
+   write('Cac uoc cua ',n,' la: { ');
+   for i:=1 to n do
+      if n mod i=0 then write(i,' ');
+   write('}');
+readln
+end.
 
 //Tìm ƯCLN và BCNN của 2 số x và y
 
