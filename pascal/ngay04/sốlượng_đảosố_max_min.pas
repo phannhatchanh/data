1498ab0b2fae3673bb04ca3272b587379df4c8e4
Chánh <phannhatchanh@gmail.com>
2022-03-03 20:05:16 +0700

    Update sốlượng_đảosố_max_min.pas

--- pascal/ngay04/sốlượng_đảosố_max_min.pas
+++ pascal/ngay04/sốlượng_đảosố_max_min.pas
@@ -9,5 +9,12 @@
         - Lớn nhất: 9; Nhỏ nhất: 0;
 }
 
-
-
+Cách giải:
+  - Nhập N
+  a) length(N)
+  b) Cho i chạy từ length(N) đến 1 => In ra N[i]
+  c) min='9'; max='0';
+     - chạy từ 1 đến length(N)
+       + Nếu max<N[i] thì max=N[i]
+       + Nếu min>N[i] thì min=N[i]
+     - In ra max và min
