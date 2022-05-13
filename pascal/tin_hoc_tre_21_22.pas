5494c8c20e46c3b2f4c1f1347331d70019fd5f77
Chánh <phannhatchanh@gmail.com>
2022-05-13 10:45:42 +0700

    Update tin_hoc_tre_21_22.pas

--- pascal/tin_hoc_tre_21_22.pas
+++ pascal/tin_hoc_tre_21_22.pas
@@ -15,6 +15,28 @@ begin
 end.
 
 { Câu 2: Viết chương trình cho phép thực hiện rút gọn phân số }
+uses crt;
+var tu,mau:integer;
+Function UCLN(a,b:integer):integer;
+  var r: integer;
+  begin
+    r:= a mod b;
+    while r <> 0 do
+    begin
+      a:= b;
+      b:= r;
+      r:=a mod b;
+    end;
+  UCLN:=b;
+  end;
+
+begin
+clrscr;
+    write('Nhap tu: '); readln(tu);
+    write('Nhap mau: '); readln(mau);
+    write('Ket qua rut gon: ',tu,'/',mau,'=',tu div UCLN(tu,mau),'/',mau div UCLN(tu,mau));
+readln
+end.
 
 { Câu 3: Viết chương trình nhập vào một chuỗi ký tự, kiểm tra xem nó có đối xứng hay không (Ví dụ: Chuỗi đối xứng RADAR, MARDAM) }
 
